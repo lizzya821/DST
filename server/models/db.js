3b438487c611d7a7e927374e1b6d13e6c13cb17d
lizzya821 <lizzya821@gmail.com>
2020-02-28 08:43:45 -0600

    process.env added

--- server/models/db.js
+++ server/models/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize")
-let db = new Sequelize('postgres://localhost:5432/DST', {
+require("dotenv").config()
+let db = new Sequelize( process.env.DATABASE_URL || 'postgres://localhost:5432/DST', {
     logging: false 
   });
 
